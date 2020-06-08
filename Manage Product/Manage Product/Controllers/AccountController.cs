0d3046954e9a0f9650a7716be46784b6e8dc818a
Dios Vo <vtmn1212@gmail.com>
2020-06-08 13:54:58 +0700

    method
    user registration

--- Manage Product/Manage Product/Controllers/AccountController.cs
+++ Manage Product/Manage Product/Controllers/AccountController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MP.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Manage_Product.Controllers
 {
@@ -14,6 +19,39 @@ namespace Manage_Product.Controllers
         {
             _userManager = userManager;
             _signInManager = signInManager;
+        }
+
+        [HttpPost("action")]
+        public async Task<IActionResult> Register([FromBody] RegisterView formdata) 
+        {
+            // Hold all the errors related to registration
+            List<string> errorList = new List<string>();
+
+            var user = new IdentityUser
+            {
+                Email = formdata.Email,
+                UserName = formdata.Username,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var result = await _userManager.CreateAsync(user, formdata.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+
+                // Sending Confirmation Email
+                return Ok(new { userName = user.UserName, email = user.Email, status = 1, message = "Registration Successful" });
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    errorList.Add(error.Description);
+                }
+            }
+            return BadRequest(new JsonResult(errorList));
         }
     }
 }
