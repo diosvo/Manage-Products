4cd31ba931ef87ab5b94122a98bd162a630b4bd8
Dios Vo <vtmn1212@gmail.com>
2020-06-08 12:00:07 +0700

    models
    login, register and product

---
+++ Manage Product/MP.Domain/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MP.Domain.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Description { get; set; }
+
+        [Required]
+        public bool OutOfStock { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+    }
+}
---
+++ Manage Product/MP.Domain/Models/RegisterView.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MP.Domain.Models
+{
+    public class RegisterView
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
