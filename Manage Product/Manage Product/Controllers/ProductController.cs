ddac008119b30610d17788fdbaf11f9d61ede296
Dios Vo <vtmn1212@gmail.com>
2020-06-08 15:44:55 +0700

    product
    controller

---
+++ Manage Product/Manage Product/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MP.Database;
+using System.Linq;
+
+namespace Manage_Product.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db; 
+        
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/value
+        [HttpGet("[action]")]
+        public IActionResult GetProducts()
+        {
+            return Ok(_db.Products.ToList());
+        }
+    }
+}
