b3fb444b8a7778a6488207b23d617f4697e6de9a
Dios Vo <vtmn1212@gmail.com>
2020-06-10 11:55:00 +0700

    design
    Product List Component

--- Manage Product/Manage Product/ClientApp/src/app/app-routing.module.ts
+++ Manage Product/Manage Product/ClientApp/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
-import { ProductComponent } from './pages/product/product.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'product', component: ProductComponent },
+  { path: 'product-details', component: ProductDetailsComponent },
+  { path: 'product-list', component: ProductListComponent },
   { path: '**', redirectTo: '/home' }
 ];
 
--- Manage Product/Manage Product/ClientApp/src/app/app.module.ts
+++ Manage Product/Manage Product/ClientApp/src/app/app.module.ts
@@ -3,19 +3,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './pages/home/home.component';
-import { ProductComponent } from './pages/product/product.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    ProductComponent
+    ProductDetailsComponent,
+    ProductListComponent,
   ],
   imports: [
     BrowserModule,
